fbad77038c5acd9c63265fb71243ca8e6859a9cc
yycer <15900617310@163.com>
2020-08-15 10:22:17 +0800

    Just Test

---
+++ src/main/java/com/learnspringcloud/demo/Test.java
@@ -0,0 +1,11 @@
+package com.learnspringcloud.demo;
+
+/**
+ * @author: Yao Frankie
+ * @date: 2020/8/15 10:22
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
